327a96a4d2fc05329b2fe8d61a93a0f88fd8cec3
andrewtdiz <andrewdizenzojhu@gmail.com>
2020-05-29 17:26:04 -0400

    laptop changes

--- src/components/dashboard/dashboardsidebar.vue
+++ src/components/dashboard/dashboardsidebar.vue
@@ -4,20 +4,22 @@
             enter-active-class="__animated "
         >
         <div class="absolute top-0 left-0 w-72 px-4 flex flex-col items-start" v-if="sidebarDelay">
-        </transition>
-        <div class="w-full flex flex-col bg-blue-100 border rounded border-blue-200 items-start px-4 py-2 my-2">
-            <p class="text-xs py-2 text-left">Add tags to your cards to organize them by concept, lecture, etc.</p>
-            <p class="text-xs text-left cursor-pointer hover:underline text-blue-700">Learn more here</p>
-
-        </div>
-        <div class="flex flex-col items-stretch w-full">
-            <p class="text-sm uppercase font-bold text-gray-700 text-left py-2">Tags</p>
             
-            <div v-for="(tag,ind) in tags" :key="ind+20" class="select-none flex flex-col items-stretch" :class="ind!=0 ? 'mt-6' : ''">
-            <TagDropDown :tag="tag" :depth="1"/>
-            </div>
+            <div class="w-full flex flex-col bg-blue-100 border rounded border-blue-200 items-start px-4 py-2 my-2">
+                <p class="text-xs py-2 text-left">Add tags to your cards to organize them by concept, lecture, etc.</p>
+                <p class="text-xs text-left cursor-pointer hover:underline text-blue-700">Learn more here</p>
 
+            </div>
+            <div class="flex flex-col items-stretch w-full">
+                <p class="text-sm uppercase font-bold text-gray-700 text-left py-2">Tags</p>
+                
+                <div v-for="(tag,ind) in tags" :key="ind+20" class="select-none flex flex-col items-stretch" :class="ind!=0 ? 'mt-6' : ''">
+                <TagDropDown :tag="tag" :depth="1"/>
+                </div>
+
+            </div>
         </div>
+        </transition>
     </div>
 </template>
 
